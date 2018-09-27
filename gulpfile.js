2fc419b28b7651a6930998408bcb9df22426fa7e
Chrismarcel <cmarcelj@gmail.com>
2018-09-27 18:39:54 +0100

    feat: Implement home page

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
 const gulp = require("gulp");
+const minifyCSS = require("gulp-clean-css");
+const concat = require("gulp-concat");
+
+gulp.task("compress", () => {
+  return gulp
+    .src("./src/css/*.css")
+    .pipe(concat("styles.min.css"))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest("./dist/css/"));
+});
+
+gulp.task("watch", () => {
+  gulp.watch("./src/css/*.css", ["compress"]);
+});
